9d4b428c37554c694011dc023d927d97a446ae86
Mazhar-54321 <syedmazharali742@gmail.com>
2025-05-13 18:06:10 +0530

    added fetchArrap Api code

---
+++ script.js
@@ -0,0 +1,61 @@
+var tbody = document.getElementById("tbody")
+function fetchData(){
+ promise1().then((res)=>{
+    return promise2()
+ }).then((res)=>{
+    return promise3()
+ }).then((res)=>{
+
+ })
+}
+function promise1(){
+ return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+       fetch('https://dummyjson.com/posts').then((res)=>{
+        return res.json()
+       }).then((res)=>{
+        let data=res.posts.map((el)=>{
+            let tr =Object.entries(el).map((el1)=>`<td>${el1[0]}:${JSON.stringify(el1[1])}</td>`).join("");
+           return  `<tr>${tr}</tr>`
+        }).join("")
+        tbody.innerHTML+=data;
+        console.log(data,"data")
+        resolve(res);
+       })
+    },1000)
+ })
+}
+function promise2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+           fetch('https://dummyjson.com/products').then((res)=>{
+            return res.json()
+           }).then((res)=>{
+            let data=res.products.map((el)=>{
+                let tr =Object.entries(el).map((el1)=>`<td>${el1[0]}:${JSON.stringify(el1[1])}</td>`).join("");
+               return  `<tr>${tr}</tr>`
+            }).join("")
+            tbody.innerHTML+=data;
+            console.log(data,"data")
+            resolve(res);
+           })
+        },2000)
+     })
+}
+function promise3(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+           fetch('https://dummyjson.com/todos').then((res)=>{
+            return res.json()
+           }).then((res)=>{
+            let data=res.todos.map((el)=>{
+                let tr =Object.entries(el).map((el1)=>`<td>${el1[0]}:${JSON.stringify(el1[1])}</td>`).join("");
+               return  `<tr>${tr}</tr>`
+            }).join("")
+            tbody.innerHTML += data;
+            console.log(data,"data")
+            resolve(res);
+           })
+        },3000)
+     })
+}
